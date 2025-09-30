ed7ff09a4d33058d32ba21416f5a00d54be311ec
Abdur Rahman Toha <120556043+Atrbd-Playz@users.noreply.github.com>
2025-09-30 23:29:47 +0600

    Favicon Testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <meta name="apple-mobile-web-app-title" content="Whispera" />
+    </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
